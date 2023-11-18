2ab202a6f4549217d7dcf72f38fd9d2b4af4d836
Yukun Zhan <y3zhan@uwaterloo.ca>
2023-11-18 12:08:26 -0500

    fix bug 3

--- swim_api.py
+++ swim_api.py
@@ -71,6 +71,7 @@ def perform_action(state, action):
     data = s.recv(1024)
     max_server = int(str(data.decode("utf-8")))
     done = False
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn = s.connect((host, port))
     if action[0]=="add":
@@ -85,6 +86,8 @@ def perform_action(state, action):
         else:
             s.sendall(b'remove_server')
             data = s.recv(1024)
+
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn = s.connect((host, port))
     if action[1] > 0:
@@ -98,7 +101,7 @@ def perform_action(state, action):
     elif action[1] < 0:
         if float(dimmer) - 0.1 > 0:
             print(str.encode(str(float(dimmer) - 0.1)))
-            s.sendall(b'set_dimmer' + str.encode(str(float(dimmer) - 0.1)))
+            s.sendall(b'set_dimmer ' + str.encode(str(float(dimmer) - 0.1)))
             data = s.recv(1024)
         else:
             done = True
