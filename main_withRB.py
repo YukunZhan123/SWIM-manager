21491bcfc69ebccb3a920eb4348c7239da10c8b6
Yukun Zhan <y3zhan@uwaterloo.ca>
2023-11-19 14:03:00 -0500

    Add replay buffer

--- main_withRB.py
+++ main_withRB.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import random
-from swim_api import get_system_state, perform_action  # Replace with your actual SWIM API calls
+from swim_api import get_system_state, perform_action
 
 torch.autograd.set_detect_anomaly(True)
 
@@ -22,12 +22,11 @@ class Actor(nn.Module):
             nn.ReLU(),
             nn.Linear(256, action_size)
         )
-        self.log_softmax = nn.LogSoftmax(dim=-1)  # Use LogSoftmax
+        self.log_softmax = nn.LogSoftmax(dim=-1)
 
     def forward(self, state):
         x = self.network(state)
-        return self.log_softmax(x)  # Return log probabilities
-
+        return self.log_softmax(x)
 
 # Define Critic network architecture
 class Critic(nn.Module):
@@ -40,68 +39,72 @@ class Critic(nn.Module):
             nn.ReLU(),
             nn.Linear(256, 1)
         )
-        
+
     def forward(self, state):
         return self.network(state)
+
+# Replay Buffer
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+
+    def push(self, state, action, reward, next_state, done):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = (state, action, reward, next_state, done)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.buffer, batch_size)
+
+    def __len__(self):
+        return len(self.buffer)
 
 # Actor-Critic Manager
 class ActorCriticManager:
     def __init__(self, state_size, action_size, epsilon, actor_lr=0.005, critic_lr=0.005):
         self.actor = Actor(state_size, action_size)
         self.critic = Critic(state_size)
-        # Set custom learning rates for the actor and critic optimizers
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=actor_lr)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=critic_lr)
         self.epsilon = epsilon
-        self.gamma = 0.99  # Discount factor for future rewards
+        self.gamma = 0.99
 
     def select_action(self, state):
         state_tensor = torch.FloatTensor(state)
         with torch.no_grad():
             action_probs = self.actor(state_tensor).numpy()
         if random.random() < self.epsilon:
-            print("random explore")
             action = np.random.choice(len(action_probs))
             self.epsilon *= 0.99
         else:
-            print("choose best action")
             action = np.argmax(action_probs)
         return action
 
-    def update(self, state, action, reward, next_state, done):
-        print("reward ", reward)
-        state_tensor = torch.FloatTensor(state)
-        next_state_tensor = torch.FloatTensor(next_state)
-        reward_tensor = torch.FloatTensor([reward])
-        done_tensor = torch.FloatTensor([done])
+    def update_batch(self, states, actions, rewards, next_states, dones):
+        states = torch.FloatTensor(states)
+        actions = torch.LongTensor(actions).unsqueeze(1)
+        rewards = torch.FloatTensor(rewards)
+        next_states = torch.FloatTensor(next_states)
+        dones = torch.FloatTensor(dones)
 
-        # Calculate the critic's estimate of the state's value and next state's value
-        value = self.critic(state_tensor)
-        next_value = self.critic(next_state_tensor).detach()
+        values = self.critic(states)
+        next_values = self.critic(next_states).detach()
+        td_targets = rewards + self.gamma * next_values * (1 - dones)
 
-        # Calculate the temporal difference target
-        td_target = reward_tensor + self.gamma * next_value * (1 - done_tensor)
-
-        # Compute the critic loss
-        critic_loss = (td_target - value).pow(2).mean()
-        print("critic_loss ", critic_loss)
+        critic_loss = (td_targets - values).pow(2).mean()
         self.critic_optimizer.zero_grad()
-        critic_loss.backward(retain_graph=True)  # Default is retain_graph=False
+        critic_loss.backward()
         self.critic_optimizer.step()
 
-        # Compute the actor loss
-        action_tensor = torch.LongTensor([action]).unsqueeze(1)
-        # Compute the actor loss using log probabilities
-        log_probs = self.actor(state_tensor).unsqueeze(0)  # Log probabilities
-        action_log_probs = log_probs.gather(1, action_tensor).squeeze(1)
-        actor_loss = -action_log_probs * (td_target - value.detach()).squeeze()
-        print("actor_loss ", actor_loss)
-
-        # Reset gradients and perform a backward pass for the actor
+        log_probs = self.actor(states)
+        action_log_probs = log_probs.gather(1, actions).squeeze(1)
+        actor_loss = -action_log_probs * (td_targets - values.detach()).squeeze()
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         self.actor_optimizer.step()
-
 
 # Utility function
 def calculate_utility(state):
@@ -127,7 +130,6 @@ def calculate_utility(state):
     return utility
 
 
-
 def reset():
     print("resetting environment")
     host = "127.0.0.1"
@@ -143,63 +145,42 @@ if len(sys.argv) == 2:
 else:
     epsilon = 1
 
-# Real-time execution loop
-state_size = 5  # Size of the state vector
+# Main execution loop
+state_size = 5
 action_choices = [["add", 0], ["remove", 0], ["nothing", 0.25], ["nothing", -0.25], ["nothing", 0], ["add", 0.25], ["add", -0.25], ["remove", 0.25], ["remove", -0.25]]
-action_size = 9  # Number of actions
+action_size = 9
+replay_buffer = ReplayBuffer(capacity=10000)
+batch_size = 32
 
-
-# Define paths to your saved model files
-actor_model_path = 'actor.pth'
-critic_model_path = 'critic.pth'
-
-# Initialize your manager
 manager = ActorCriticManager(state_size, action_size, epsilon)
 
-# Check if both the actor and critic model files exist
-if os.path.exists(actor_model_path) and os.path.exists(critic_model_path):
-    print("Loading saved model weights.")
-    manager.actor.load_state_dict(torch.load(actor_model_path))
-    manager.critic.load_state_dict(torch.load(critic_model_path))
+if os.path.exists('actor.pth') and os.path.exists('critic.pth'):
+    manager.actor.load_state_dict(torch.load('actor.pth'))
+    manager.critic.load_state_dict(torch.load('critic.pth'))
 else:
     print("No saved model weights found, initializing new models.")
 
-
 reset()
-
-# Add a counter for iterations
 iteration_counter = 0
 
-while True:  # Replace with the condition appropriate for your application
-    # Monitor
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    print("it: ", iteration_counter)
-
-    if iteration_counter % 30 == 0:
-        # Save the actor and critic networks
-        torch.save(manager.actor.state_dict(), f'actor.pth')
-        torch.save(manager.critic.state_dict(), f'critic.pth')
-
+while True:  # Replace with your specific condition
     state = get_system_state()
-
-    # Plan
     action = manager.select_action(state)
-    print(action)
-
-
-    # Execute
-    done = perform_action(state, action_choices[action])  # Implement this function
+    done = perform_action(state, action_choices[action])
     time.sleep(5)
     next_state = get_system_state()
-
-    # Analyze
     reward = calculate_utility(next_state)
 
-    # Update the manager
-    manager.update(state, int(action), reward, next_state, done)
+    replay_buffer.push(state, action, reward, next_state, done)
+    if len(replay_buffer) > batch_size:
+        experiences = replay_buffer.sample(batch_size)
+        states, actions, rewards, next_states, dones = zip(*experiences)
+        manager.update_batch(states, actions, rewards, next_states, dones)
 
-    iteration_counter += 1  # Increment the counter
+    iteration_counter += 1
+    if iteration_counter % 30 == 0:
+        torch.save(manager.actor.state_dict(), 'actor.pth')
+        torch.save(manager.critic.state_dict(), 'critic.pth')
 
-    if done:  # Implement the logic to determine if the episode has ended
+    if done:
         reset()
-        continue
