fc2e552b8eab12a2a0223a977b177dd0211a2b1f
Yukun Zhan <y3zhan@uwaterloo.ca>
2023-11-19 10:17:25 -0500

    fix bug 4

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class Critic(nn.Module):
 
 # Actor-Critic Manager
 class ActorCriticManager:
-    def __init__(self, state_size, action_size, epsilon=0.5, actor_lr=0.005, critic_lr=0.005):
+    def __init__(self, state_size, action_size, epsilon=0.1, actor_lr=0.005, critic_lr=0.005):
         self.actor = Actor(state_size, action_size)
         self.critic = Critic(state_size)
         # Set custom learning rates for the actor and critic optimizers
